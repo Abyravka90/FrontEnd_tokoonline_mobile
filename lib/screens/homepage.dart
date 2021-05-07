67a197a8175755b34ddaf881c59af7d202e65a51
Abyravka90 <asepcahya.nugraha75@gmail.com>
2021-05-07 22:30:22 +0700

    initial Commit CRUD Data from FE to BE

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,154 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:toko_online/screens/add_product.dart';
+import 'package:toko_online/screens/edit_product.dart';
+import 'package:toko_online/screens/product_detail.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key key,
+  }) : super(key: key);
+
+  final String url = "http://10.0.2.2:8000/api/products";
+  // http://10.0.2.2:8000/api/products
+
+  Future getProducts() async {
+    var response = await http.get(Uri.parse(url));
+    print(json.decode(response.body));
+    return json.decode(response.body);
+  }
+
+  Future deleteProducts(String productId) async {
+    String url = "http://10.0.2.2:8000/api/products/" + productId;
+    var response = await http.delete(Uri.parse(url));
+    print(json.decode(response.body));
+    return json.decode(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getProducts();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddProduct()));
+        },
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: Text('Asep Cahya Store', style: TextStyle(fontSize: 25)),
+      ),
+      body: FutureBuilder(
+        future: getProducts(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // return Text('Data Oke');
+            return ListView.builder(
+                itemCount: snapshot.data['data'].length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 180,
+                    child: Card(
+                      elevation: 5,
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ProductDetail(
+                                            product: snapshot.data['data']
+                                                [index],
+                                          )));
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15.0)),
+                              padding: EdgeInsets.all(5),
+                              height: 100,
+                              width: 100,
+                              child: Image.network(
+                                  snapshot.data['data'][index]['image_url']),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.all(10.0),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                        snapshot.data['data'][index]['name'],
+                                        style: TextStyle(
+                                            fontSize: 20.0,
+                                            fontWeight: FontWeight.bold)),
+                                  ),
+                                  Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(snapshot.data['data'][index]
+                                        ['description']),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          GestureDetector(
+                                              onTap: () {
+                                                Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            EditProduct(
+                                                              product: snapshot
+                                                                          .data[
+                                                                      'data']
+                                                                  [index],
+                                                            )));
+                                              },
+                                              child: Icon(Icons.edit)),
+                                          GestureDetector(
+                                              onTap: () {
+                                                deleteProducts(snapshot
+                                                        .data['data'][index]
+                                                            ['id']
+                                                        .toString())
+                                                    .then((value) {
+                                                  ScaffoldMessenger.of(context)
+                                                      .showSnackBar(SnackBar(
+                                                          content: Text(
+                                                              "Data Berhasil Di Hapus!")));
+                                                });
+                                              },
+                                              child: Icon(Icons.delete)),
+                                        ],
+                                      ),
+                                      Text(snapshot.data['data'][index]
+                                          ['price']),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          } else {
+            return Text('Data Error Check the server');
+          }
+        },
+      ),
+    );
+  }
+}
